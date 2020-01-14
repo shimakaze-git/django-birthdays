24ff34718e1966acd8339ad08de4592b4024167e
shimakaze-git <shimakaze.soft+github@googlemail.com>
2020-01-14 12:18:55 +0900

    refactor: register pypi.

---
+++ jp_birthday/__init__.py
@@ -0,0 +1,9 @@
+
+__copyright__    = 'Copyright (C) 2020 shimakaze-git'
+__version__      = '0.0.1'
+__license__      = 'MIT'
+__author__       = 'shimakaze-git'
+__author_email__ = 'shimakaze.soft+github@googlemail.com'
+__url__          = 'http://github.com/shimakaze-git/django-jp-birthday'
+
+__all__ = ['fields', 'managers', 'models']
