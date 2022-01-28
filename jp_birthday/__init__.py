df0f650e75a6a03628d4aaa9d5749b3a707c8215
shimakaze-git <shimakaze.soft+github@googlemail.com>
2022-01-28 09:50:15 +0900

    feat: 0.1.1に変更する.

--- jp_birthday/__init__.py
+++ jp_birthday/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """shimakaze-git"""
 __email__ = "shimakaze.soft+github@googlemail.com"
-__version__ = "0.1.2"
+__version__ = "0.1.1"
 
 __copyright__ = "Copyright (C) 2020 shimakaze-git"
 __license__ = "MIT"
