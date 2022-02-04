9a6aadfc7d7395d0475ceb233b81ca6c3c49971d
shimakaze-git <shimakaze.soft+github@googlemail.com>
2022-02-04 16:05:14 +0900

    refactor: get_upcoming_birthdaysを再実装.
    中身のロジックを修正していく.

--- jp_birthday/managers.py
+++ jp_birthday/managers.py
@@ -1,5 +1,7 @@
-from django.db import models
-from django.db.models import Case, When
+from django.conf import settings
+
+from django.db import models, router, connection
+from django.db.models import Case, When, Value, IntegerField
 
 # from django.db.models.query_utils import Q
 
@@ -13,6 +15,22 @@ import unicodedata
 import jaconv
 
 
+class JpBirthdayQuerySet(models.QuerySet):
+    def __init__(self, model=None, query=None, using=None, hints=None):
+        super().__init__(model, query, using, hints)
+
+    def order_by_ids_filter(self, ids: list):
+        cases = [When(id=id, then=Value(i + 1)) for i, id in enumerate(ids)]
+
+        birthdays = (
+            self.filter(id__in=ids)
+            .annotate(md_order=Case(*cases, output_field=IntegerField()))
+            .order_by("md_order")
+        )
+
+        return birthdays
+
+
 class JpBirthdayManager(models.Manager):
     """[summary]
 
@@ -21,6 +39,9 @@ class JpBirthdayManager(models.Manager):
     """
 
     j2w = jeraconv.J2W()
+
+    def get_queryset(self):
+        return JpBirthdayQuerySet(self.model, using=self._db)
 
     def _check_language(self, string):
         l_type = ""
@@ -89,7 +110,7 @@ class JpBirthdayManager(models.Manager):
     def get_upcoming_birthdays(
         self, days=30, after=None, include_day=True, order=True, reverse=False
     ):
-        """[summary]
+        """get_upcoming_birthdays
 
         Args:
             days (int, optional): [description]. Defaults to 30.
@@ -101,10 +122,10 @@ class JpBirthdayManager(models.Manager):
         Returns:
             [type]: [description]
         """
-        # 今後の誕生日
 
-        birthdays = self.filter()
-        birthdays_list = [b.birthday_tm_yday for b in birthdays]
+        db_route = router.db_for_read(self)
+        engine = settings.DATABASES[db_route]["ENGINE"]
+        db_table_name = self.model._meta.db_table
 
         if after:
             after = datetime.combine(after, time())
@@ -112,36 +133,127 @@ class JpBirthdayManager(models.Manager):
         else:
             after = datetime.now()
 
-        days += 1
         if not include_day:
-            after = after + timedelta(days=1)
+            after += timedelta(days=1)
             days -= 1
+        days = str(days)
 
-        after_list = [
-            (after + timedelta(days=i)).date().timetuple().tm_yday for i in range(days)
-        ]
+        after = after.date()
+        after_str = "'{0}'".format(after)
 
-        idx_list = []
-        for a_t in after_list:
-            if birthdays_list.count(None) == len(birthdays_list):
-                break
+        sql = "select {0}.*, ".format(db_table_name)
 
-            for _ in birthdays_list:
-                if a_t in birthdays_list:
-                    n = birthdays_list.index(a_t)
-                    # print("n", n, a_t, birthdays_list[n])
-                    birthdays_list[n] = None
-                    idx_list.append(n)
+        between_sql = "between "
+        if "sqlite3" in engine:
+            sql += "strftime('%m-%d', birthday) as md "
 
-        pk_list = [birthdays[idx].pk for idx in idx_list]
+            between_sql += "strftime('%m-%d', {0}) and ".format(after_str)
+            between_sql += (
+                "strftime('%m-%d', date({0}, 'localtime', '+{1} day'))".format(
+                    after_str, days
+                )
+            )
 
-        birthdays = birthdays.filter(pk__in=pk_list)
+            # sql += " order by birthday asc;"
+        else:
+            pass
+
+        sql += "from {0} where md ".format(db_table_name)
+        sql += between_sql
+
         if order:
-            birthdays = birthdays.order_by(*("birthday",))
+            # print("order")
+            # birthdays = birthdays.order_by(*("birthday",))
+            sql += " order by md asc;"
 
-        if reverse:
-            birthdays = birthdays.reverse()
-        return birthdays
+        print("sql", sql)
+
+        cursor = connection.cursor()
+        try:
+            cursor.execute(sql)
+
+            columns = [col[0] for col in cursor.description]
+            ids = [dict(zip(columns, row))["id"] for row in cursor.fetchall()]
+            # print("ids", ids)
+
+            birthdays = self.get_queryset().order_by_ids_filter(ids)
+
+            return birthdays
+        finally:
+            cursor.close()
+
+        # order = False
+        # print("order", order)
+        # print("reverse", reverse)
+        # reverse = True
+
+        # order = True
+        # reverse = True
+
+        # if order:
+        #     print("order")
+        #     birthdays = birthdays.order_by(*("birthday",))
+
+        # if reverse:
+        #     print("reverse")
+        #     birthdays = birthdays.reverse()
+
+    # def get_upcoming_birthdays(
+    #     self, days=30, after=None, include_day=True, order=True, reverse=False
+    # ):
+    #     """[summary]
+
+    #     Args:
+    #         days (int, optional): [description]. Defaults to 30.
+    #         after ([type], optional): [description]. Defaults to None.
+    #         include_day (bool, optional): [description]. Defaults to True.
+    #         order (bool, optional): [description]. Defaults to True.
+    #         reverse (bool, optional): [description]. Defaults to False.
+
+    #     Returns:
+    #         [type]: [description]
+    #     """
+    #     # 今後の誕生日
+
+    #     birthdays = self.filter()
+    #     birthdays_list = [b.birthday_tm_yday for b in birthdays]
+
+    #     if after:
+    #         after = datetime.combine(after, time())
+    #         after = pytz.timezone("Asia/Tokyo").localize(after)
+    #     else:
+    #         after = datetime.now()
+
+    #     days += 1
+    #     if not include_day:
+    #         after = after + timedelta(days=1)
+    #         days -= 1
+
+    #     after_list = [
+    #         (after + timedelta(days=i)).date().timetuple().tm_yday for i in range(days)
+    #     ]
+
+    #     idx_list = []
+    #     for a_t in after_list:
+    #         if birthdays_list.count(None) == len(birthdays_list):
+    #             break
+
+    #         for _ in birthdays_list:
+    #             if a_t in birthdays_list:
+    #                 n = birthdays_list.index(a_t)
+    #                 # print("n", n, a_t, birthdays_list[n])
+    #                 birthdays_list[n] = None
+    #                 idx_list.append(n)
+
+    #     pk_list = [birthdays[idx].pk for idx in idx_list]
+
+    #     birthdays = birthdays.filter(pk__in=pk_list)
+    #     if order:
+    #         birthdays = birthdays.order_by(*("birthday",))
+
+    #     if reverse:
+    #         birthdays = birthdays.reverse()
+    #     return birthdays
 
     def get_birthdays(self, day=None):
         """[summary]
--- tests/test_managers.py
+++ tests/test_managers.py
@@ -1,9 +1,16 @@
+from datetime import date, datetime
+
+from django.db import connection
+
+# from django.db import backends
+
 from django.test import TestCase
+from django.conf import settings
 
 from tests.models import ModelTest
 
 
-class InitTest(TestCase):
+class BirthdayTestManagers(TestCase):
     @classmethod
     def setup_class(self):
         """テストclass実行の前処理"""
@@ -11,14 +18,46 @@ class InitTest(TestCase):
         print("setup_class")
 
     def setUp(self):
-        self.meiji = ["1905-1-2", "1905-1-1", "1905-2-1", "1905-2-2"]
-        self.showa = ["1980-7-7", "1975-1-1"]
-        self.heisei = ["2000-01-02", "2001-01-01", "2002-12-31"]
+        self.meiji = ["1905-1-2", "1905-1-1", "1905-2-1", "1905-2-2", "1905-1-30"]
+        self.showa = ["1980-7-7", "1975-1-1", "1970-2-15"]
+        self.heisei = ["2000-01-02", "2001-01-01", "2002-12-31", "2021-2-20"]
 
         self.birthdays = self.meiji + self.showa + self.heisei
         for birthday in self.birthdays:
             model_test = ModelTest(birthday=birthday)
             model_test.save()
+
+    def test_get_upcoming_birthdays(self):
+
+        settings.DEBUG = True
+
+        jan1 = date(year=2010, month=1, day=1)
+        print("jan1", jan1)
+
+        # 1月の要素だけ抽出
+        count = [int(birthday.split("-")[1]) for birthday in self.birthdays].count(1)
+        print("count", count)
+
+        # get_upcoming_birthdays = ModelTest.objects.get_upcoming_birthdays(30, jan1, False)
+        get_upcoming_birthdays = ModelTest.objects.get_upcoming_birthdays(30, jan1)
+        self.assertEqual(get_upcoming_birthdays.count(), count)
+
+        # self.assertEqual(
+        #     ModelTest.objects.get_upcoming_birthdays(30, jan1, False).count(), 1
+        # )
+
+        # data = ModelTest.objects.raw(sql)
+        # for d in data:
+        #     print("d", d)
+
+        print("----" * 15)
+
+        settings.DEBUG = False
+
+        for query in connection.queries:
+            print(query["sql"])
+            print("----" * 5)
+        print(len(connection.queries))
 
     def test_order_by_birthday(self):
         pks1 = [obj.pk for obj in ModelTest.objects.order_by("birthday")]
