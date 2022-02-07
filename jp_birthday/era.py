58c0ec9440cea238d363e1d955e73e8677d40758
shimakaze-git <shimakaze.soft+github@googlemail.com>
2022-02-07 14:25:44 +0900

    fix: 再度3.10を指定する.

--- jp_birthday/era.py
+++ jp_birthday/era.py
@@ -4,3 +4,16 @@ from jeraconv import jeraconv
 class JpEra:
     def __init__(self, *args, **kwargs):
         self.j2w = jeraconv.J2W()
+
+    def __convert_to_jp_era(self, year: int, month: int, day: int) -> dict:
+        """[summary]
+
+        Args:
+            year (int): [description]
+            month (int): [description]
+            day (int): [description]
+
+        Returns:
+            dict: [description]
+        """
+        pass
